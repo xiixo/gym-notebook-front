3b448e262ca7e9c7feb6f38999a0212e2c6b4cfc
xiixo <robertzakfox@gmail.com>
2025-11-09 11:28:49 +0700

    feat(): playin with css and themes

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router";
 import styles from "./HomeScreen.module.css";
 
+import WebApp from "@twa-dev/sdk";
+
 export default function HomeScreen() {
   return (
     <>
       <h1 className={styles.head}>Home</h1>
-      <p>Hello</p>
+      <p>Hello {WebApp?.initDataUnsafe?.user?.first_name}</p>
+      <p>Hello {JSON.stringify(WebApp?.safeAreaInset)}</p>
+      <p>Hello {JSON.stringify(WebApp?.contentSafeAreaInset)}</p>
       <Link to="/workout">Go to workout</Link>
     </>
   );
